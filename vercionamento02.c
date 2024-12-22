997d30bf08934d9eb323c962f50f0c3710339122
LuisEduardo <eduardo38sao@gmail.com>
2024-12-21 22:36:19 -0300

    Alteração do funcionamento do código, onde foi removida a variavel resultado com os resultados da s operações sendo feitos nos printf, para tornar mais rapido o código.

--- vercionamento02.c
+++ vercionamento02.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 void main(){
-    int numero01, numero02, resultado;
+    int numero01, numero02;
 
     printf("Diginte o primeiro valor: ");
     scanf("%d", &numero01); //O valor que o usuario digitar vai ser passado para essa variavel
     printf("Diginte o primeiro valor: ");
     scanf("%d", &numero02);
 
-    resultado = numero01 + numero02;
-
     system("cls");
-    printf("O resultado da soma dos valores E: %d\n", resultado);
+    printf("O resultado da soma dos valores E: %d\n", numero01 + numero02);
+    printf("O resultado da subtracao dos valores E: %d\n", numero01 - numero02);
+    printf("O resultado da divisao dos valores E: %d\n", numero01 / numero02);
+    printf("O resultado da multiplicacao dos valores E: %d\n", numero01 * numero02);
 }
